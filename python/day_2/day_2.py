6d819d172a8800cd32f165689ac29e8acea2a8b9
Moritz <mail@moritzhorn.de>
2025-12-03 01:02:11 +0100

    fixed error in list comprehension

--- python/day_2/day_2.py
+++ python/day_2/day_2.py
@@ -9,7 +9,7 @@ def solution():
             interval = idrange.split('-')
 
             # Get all IDs of a range that can be equally divided in two halves to test them
-            for id in [i for i in range(int(interval[0]), int(interval[1]) + 1) if i % 2 == 0]:
+            for id in [i for i in range(int(interval[0]), int(interval[1]) + 1) if len(str(i)) % 2 == 0]:
                 id = str(id)
 
                 # Check if the ID is made of two repeating halves
