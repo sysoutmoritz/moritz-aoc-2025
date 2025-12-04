4a106f7526d9f40ede6366e5a3fa4b56481a8191
Moritz <mail@moritzhorn.de>
2025-12-05 00:30:36 +0100

    day 4 solved

---
+++ python/day_4/day_4_part_2.py
@@ -0,0 +1,64 @@
+import time, os
+
+def solution():
+    start_time = time.perf_counter()
+    sum = 0
+    padding_field = []
+
+    with open(os.path.join('python', 'day_4', 'input.txt'), 'r') as file:
+        file = file.read().strip().split('\n')
+
+        # Fill a padding field with dots around the original field
+        padding_field.append(['.'] * (len(file[0]) + 2))  # Top padding
+        for line in file:
+            padding_field.append(['.'] + list(line) + ['.'])  # Side paddings
+        padding_field.append(['.'] * (len(file[0]) + 2))  # Bottom padding
+
+    # Configurations before first iteration
+    field_changed = True
+    new_padding_field = padding_field.copy()
+
+    while field_changed:
+
+        # Replace the old field of papers with the updated one
+        padding_field = new_padding_field
+        new_padding_field = padding_field.copy()
+
+        # Assume nothing has changed so far
+        field_changed = False
+
+        for i in range(1, len(padding_field) - 1):
+            for j in range(1, len(padding_field[0]) - 1):
+            
+                # Exit instantly if cell is not paper
+                if padding_field[i][j] != '@':
+                    continue
+
+                count = 0
+                still_possible = True
+
+                # Check all adjacent cells
+                for x in range(-1, 2):
+
+                    # Exit early if more than 3 neighbors are found
+                    if not still_possible:
+                        break
+                    
+                    for y in range(-1, 2):
+                        if not (x == 0 and y==0) and padding_field[i + x][j + y] == '@':
+                            count += 1
+                            if count > 3:
+                                still_possible = False
+                                break
+
+                if count <= 3:
+                    sum += 1
+                    field_changed = True
+                    new_padding_field[i][j] = '.'
+
+
+    end_time = time.perf_counter()
+    print(f"Answer: {sum}")
+    print(f"Time taken: {end_time - start_time:.9f} seconds")
+if __name__ == "__main__":
+    solution()
